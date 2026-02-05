cff15a7886d8824d626d6593e7c0270aecfa432b
Raj Pranav <raj.pranav@sailpoint.com>
2026-02-05 09:28:44 +0530

    call godog package

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/raj-pranav-sp/go-puppy
 
 go 1.25.3
+
+require github.com/raj-pranav-sp/go-dog v0.0.0-20260205035445-17fc305b38c4 // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,17 @@
 package gopuppy
 
+import (
+	godog "github.com/raj-pranav-sp/go-dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Sit() string {
 	return "Sitting"
+}
+
+func BigBark() string {
+	return godog.WhenGrownUp(Bark())
 }
